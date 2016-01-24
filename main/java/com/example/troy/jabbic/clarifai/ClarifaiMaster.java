fc12249fc1246e389049e3f38456172115570749
Troy Gittelmacher <tgittelmacher@github.com>
2016-01-24 00:38:35 -0500

    clarifai call works

--- main/java/com/example/troy/jabbic/clarifai/ClarifaiMaster.java
+++ main/java/com/example/troy/jabbic/clarifai/ClarifaiMaster.java
@@ -1,27 +1,71 @@
 package com.example.troy.jabbic.clarifai;
 
+import android.net.Uri;
+
 import com.clarifai.api.ClarifaiClient;
+import com.clarifai.api.RecognitionRequest;
+import com.clarifai.api.RecognitionResult;
+import com.clarifai.api.Tag;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Troy on 1/23/2016.
  */
 public class ClarifaiMaster {
-    private ClarifaiMaster instance;
+    private static ClarifaiMaster instance;
     private ClarifaiClient client;
 
     private final String CLIENT_ID = "aMfh1A5nJkCHC_xREeVmpssK9PXSfZ054RPRlbv6";
     private final String CLIENT_SECRET = "TG6jFD5kvs01sjG0Hf4e_ANUm3-2FbAO2iq9UsuO";
     private final String ACCESS_TOKEN = "NdXfVTEb0fH7tkGjy7WXnHy6Fjzs6J";
 
-    private ClarifaiMaster() {
 
+    private ClarifaiMaster() {
+        client = new ClarifaiClient(CLIENT_ID, CLIENT_SECRET);
     }
 
-    public ClarifaiMaster getInstance() {
+    public static ClarifaiMaster getInstance() {
         if (instance == null) {
             instance = new ClarifaiMaster();
         }
         return instance;
     }
 
+    public String[] getTags(File file) {
+
+        List<RecognitionResult> results= client.recognize(new RecognitionRequest(file));
+        ArrayList<String> strings = new ArrayList<>();
+
+        for (RecognitionResult r: results) {
+            for (Tag t: r.getTags()) {
+                strings.add(t.getName());
+            }
+        }
+
+        return (String[]) strings.toArray();
+    }
+
+    public String[] getTags(URI uri) {
+        File file = new File(uri);
+        return getTags(file);
+    }
+
+    public String[] getTags(String URL) {
+        List<RecognitionResult> results= client.recognize(new RecognitionRequest(URL));
+        ArrayList<String> strings = new ArrayList<>();
+
+        for (RecognitionResult r: results) {
+            for (Tag t: r.getTags()) {
+                strings.add(t.getName());
+            }
+        }
+
+        String[] dummy = new String[strings.size()];
+        return strings.toArray(dummy);
+    }
 }
--- main/java/com/example/troy/jabbic/fragments/MainFragment.java
+++ main/java/com/example/troy/jabbic/fragments/MainFragment.java
@@ -1,18 +1,26 @@
 package com.example.troy.jabbic.fragments;
 
+import android.app.Activity;
 import android.app.Application;
 import android.app.Fragment;
+import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.troy.jabbic.R;
 import com.example.troy.jabbic.activities.MainActivity;
+import com.example.troy.jabbic.clarifai.ClarifaiMaster;
 import com.example.troy.jabbic.clarifai.ImageAdapter;
+
+import java.io.File;
+import java.net.URI;
 
 /**
  * Created by Troy on 1/23/2016.
@@ -38,9 +46,27 @@ public class MainFragment extends Fragment {
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Toast.makeText(getActivity(), "YA CLICKED SOMETHING", Toast.LENGTH_SHORT).show();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ClarifaiMaster master = ClarifaiMaster.getInstance();
+                        final String[] results = master.getTags("http://www.cise.ufl.edu/~dts/personal/who/CamelBack.jpg");
+                        if (results != null) {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    for (int i=0; i < results.length; ++i) {
+                                        Toast.makeText(getActivity(), results[i], Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }).start();
+
             }
         });
 
     }
+
 }
