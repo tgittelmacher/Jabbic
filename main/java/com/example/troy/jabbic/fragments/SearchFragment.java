6b33c642c2fc228ff33693cd198a33a364f6759d
Troy Gittelmacher <tgittelmacher@github.com>
2016-01-24 02:06:17 -0500

    Search view shows images now too.

--- main/java/com/example/troy/jabbic/fragments/SearchFragment.java
+++ main/java/com/example/troy/jabbic/fragments/SearchFragment.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.example.troy.jabbic.R;
+import com.example.troy.jabbic.clarifai.ImageAdapter;
 
 /**
  * Created by Troy on 1/23/2016.
@@ -20,5 +22,16 @@ public class SearchFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.search_view, container, false);
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        GridView gridview = (GridView) getView().findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+        //ASK TROY IF YOU NEED TO ADJUST SEARCH SCREEN PICTURE LIST PADDING, ETC
+        gridview.setPadding(gridview.getPaddingLeft(), 200, gridview.getPaddingRight(), gridview.getBottom() + 200);
     }
 }
